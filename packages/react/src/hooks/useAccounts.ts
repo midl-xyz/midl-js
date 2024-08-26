52f638a6b4df7b2daeaa237b60f64c61378bafdb
Aleksei Dmitriev <alexdmspb@gmail.com>
2024-08-26 12:11:32 +0100

    feat: update useConnect and useAccounts

--- packages/react/src/hooks/useAccounts.ts
+++ packages/react/src/hooks/useAccounts.ts
@@ -1,12 +1,17 @@
-import { useQuery } from "@tanstack/react-query";
+import {
+  useIsMutating,
+  useMutationState,
+  useQuery,
+} from "@tanstack/react-query";
 import { useMidlContext } from "~/context";
+import { ConnectMutationKey } from "~/hooks/useConnect";
 
 export const useAccounts = () => {
   const {
-    config: { currentConnection },
+    config: { currentConnection, network, chain },
   } = useMidlContext();
 
-  return useQuery({
+  const { data, status, ...rest } = useQuery({
     queryKey: ["accounts", currentConnection],
     queryFn: async () => {
       const data = await currentConnection?.getAccounts();
@@ -15,4 +20,35 @@ export const useAccounts = () => {
     enabled: !!currentConnection,
     retry: false,
   });
+
+  const [mutationState] = useMutationState({
+    filters: { mutationKey: [ConnectMutationKey] },
+  });
+
+  const isMutating = useIsMutating({
+    mutationKey: [ConnectMutationKey],
+  });
+
+  const getStatus = () => {
+    if (isMutating) {
+      return "connecting";
+    }
+
+    if (mutationState?.status === "success") {
+      return "connected";
+    }
+
+    return "disconnected";
+  };
+
+  return {
+    accounts: data,
+    connector: currentConnection,
+    isConnecting: isMutating,
+    isConnected: mutationState?.status === "success",
+    status: getStatus(),
+    network,
+    chain,
+    ...rest,
+  };
 };
