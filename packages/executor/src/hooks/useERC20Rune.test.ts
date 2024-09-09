96dd73ee04ca2bad6478f6ca6078b4027278e353
Alex D <alexdmspb@gmail.com>
2024-09-09 12:18:09 +0100

    chore: remove tests

--- packages/executor/src/hooks/useERC20Rune.test.ts
+++ packages/executor/src/hooks/useERC20Rune.test.ts
@@ -1,9 +1,7 @@
 import { describe, it } from "vitest";
-import { useERC20Rune } from "~/hooks/useERC20Rune";
 
 describe("useERC20Rune", () => {
   it.skip("should return the correct value", () => {
     // TODO: Implement test
-    const test = useERC20Rune("test", { query: { enabled: false } });
   });
 });
