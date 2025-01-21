0d3608939ee78a222151a9514972056cfe60cec9
Aleksei Dmitriev <alexdmspb@gmail.com>
2025-01-21 17:44:09 +0000

    chore: updates

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,6 +2,21 @@ import type { UserConfig } from "@commitlint/types";
 
 const Configuration: UserConfig = {
 	extends: ["@commitlint/config-conventional"],
+	rules: {
+		"scope-enum": [
+			2,
+			"always",
+			[
+				"hardhat-deploy",
+				"docs",
+				"core",
+				"config",
+				"executor",
+				"executor-react",
+				"react",
+			],
+		],
+	},
 };
 
 export default Configuration;
