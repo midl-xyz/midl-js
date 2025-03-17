5ad0649cf28106ce75dff0436680ed8decbd1a61
Aleksei Dmitriev <alexdmspb@gmail.com>
2025-03-17 18:56:53 +0000

    feat: view and send runes

--- apps/rune-etcher/src/app/page.tsx
+++ apps/rune-etcher/src/app/page.tsx
@@ -1,20 +1,12 @@
+import { MainMenu } from "@/components/main-menu";
 import { RuneForm } from "@/components/rune-form/RuneForm";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center pt-[12vh]">
-      <div className="flex space-y-8 flex-col justify-center w-full max-w-[450px]">
-        <div className="flex flex-col space-y-0 items-start">
-          <h1 className="text-3xl font-bold text-primary">
-            Bitcoin Rune Etcher
-          </h1>
-          <p className="text-lg">Etch runes on the Bitcoin blockchain</p>
-        </div>
-
-        <div>
-          <RuneForm />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex space-y-8 flex-col justify-center w-full max-w-[450px]">
+			<div>
+				<RuneForm />
+			</div>
+		</div>
+	);
 }
---
+++ apps/rune-etcher/src/components/runes-list/RunesList.tsx
@@ -0,0 +1,176 @@
+"use client";
+
+import {
+	useAccounts,
+	useEdictRune,
+	useRune,
+	useRunes,
+} from "@midl-xyz/midl-js-react";
+import { Button } from "@/components/ui/button";
+import { SendIcon } from "lucide-react";
+import {
+	Dialog,
+	DialogClose,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { parseEther, parseUnits } from "viem";
+import { useToast } from "@/hooks/use-toast";
+
+type FormValues = {
+	address: string;
+	amount: string;
+};
+
+export const RunesList = () => {
+	const { ordinalsAccount } = useAccounts();
+	const [rune, setRune] = useState<string | undefined>();
+	const { rune: runeData } = useRune({
+		// biome-ignore lint/style/noNonNullAssertion: <explanation>
+		runeId: rune!,
+		query: {
+			enabled: Boolean(rune),
+		},
+	});
+
+	const { reset, edictRune } = useEdictRune({
+		mutation: {
+			onSettled() {
+				reset();
+			},
+			onSuccess() {
+				toast({
+					title: "Rune sent",
+				});
+			},
+			onError(error) {
+				toast({
+					title: "Error",
+					description: error.message,
+					color: "error",
+				});
+			},
+		},
+	});
+
+	const { runes } = useRunes({
+		address: ordinalsAccount?.address ?? "",
+		query: {
+			enabled: Boolean(ordinalsAccount),
+		},
+		limit: 60,
+	});
+
+	const { toast } = useToast();
+
+	const onSubmit = ({ address, amount }: FormValues) => {
+		if (!runeData) {
+			return;
+		}
+
+		console.log([
+			{
+				receiver: address,
+				runeId: runeData.id,
+				amount: parseUnits(amount, runeData.divisibility),
+			},
+		]);
+
+		edictRune({
+			transfers: [
+				{
+					receiver: address,
+					runeId: runeData.id,
+					amount: parseUnits(amount, runeData.divisibility),
+				},
+			],
+			publish: true,
+		});
+	};
+
+	const { handleSubmit, register } = useForm<FormValues>();
+
+	return (
+		<Dialog>
+			<div className="p-4 bg-gray-100 rounded-lg">
+				<div className="grid grid-cols-1 gap-4">
+					{runes?.results?.map((rune) => (
+						<div
+							key={rune.rune.id}
+							className="p-4 bg-white border border-gray-200 rounded-lg shadow-sm flex flex-row justify-between"
+						>
+							<div>
+								<div className="flex flex-row gap-1 items-center">
+									<span className="text-lg text-gray-600">
+										{rune.rune.name}
+									</span>
+									<span className="text-xs text-gray-400">{rune.rune.id}</span>
+								</div>
+								<div className="text-sm text-gray-500">
+									Balance: {rune.balance}
+								</div>
+							</div>
+							<DialogTrigger asChild>
+								<Button
+									onClick={() => setRune(rune.rune.id)}
+									className="mt-2 flex items-center"
+								>
+									<SendIcon className="w-4 h-4 mr-2" />
+									Send
+								</Button>
+							</DialogTrigger>
+						</div>
+					))}
+				</div>
+			</div>
+
+			<DialogContent className="sm:max-w-[425px]">
+				<DialogHeader>
+					<DialogTitle>Send rune</DialogTitle>
+					<DialogDescription>
+						Send a rune to another wallet address
+					</DialogDescription>
+				</DialogHeader>
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<div className="grid gap-4 py-4">
+						<div className="grid grid-cols-4 items-center gap-4">
+							<Label htmlFor="address" className="text-right">
+								Address
+							</Label>
+							<Input
+								id="address"
+								{...register("address")}
+								className="col-span-3"
+								placeholder="bc1q..."
+							/>
+						</div>
+						<div className="grid grid-cols-4 items-center gap-4">
+							<Label htmlFor="amount" className="text-right">
+								Amount
+							</Label>
+							<Input
+								id="amount"
+								className="col-span-3"
+								placeholder="902.123"
+								{...register("amount")}
+							/>
+						</div>
+					</div>
+					<DialogFooter>
+						<DialogClose>
+							<Button type="submit">Send</Button>
+						</DialogClose>
+					</DialogFooter>
+				</form>
+			</DialogContent>
+		</Dialog>
+	);
+};
