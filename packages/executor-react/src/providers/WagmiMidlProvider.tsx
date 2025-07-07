15f2a89c4090f8493da894808bdbcea0f6721ed8
Alex Novak <alexdmspb@gmail.com>
2025-07-07 18:07:32 +0100

    feat: provide wagmi (#107)

--- packages/executor-react/src/providers/WagmiMidlProvider.tsx
+++ packages/executor-react/src/providers/WagmiMidlProvider.tsx
@@ -1,24 +1,35 @@
 "use client";
 
-import type { Chain } from "@midl-xyz/midl-js-executor";
+import {
+	type Chain,
+	getEVMFromBitcoinNetwork,
+	midlRegtest,
+} from "@midl-xyz/midl-js-executor";
+import { useConfig } from "@midl-xyz/midl-js-react";
 import { useEffect, useMemo } from "react";
-import { zeroAddress } from "viem";
-import { useAccount, useConnect, useSwitchChain } from "wagmi";
+import { http, zeroAddress } from "viem";
+import {
+	WagmiProvider,
+	createConfig,
+	useAccount,
+	useConnect,
+	useSwitchChain,
+} from "wagmi";
 import { mock } from "wagmi/connectors";
 import { useEVMAddress, useEVMChain } from "~/hooks";
 
 type WagmiMidlProviderProps = {
 	chain?: Chain;
+	children?: React.ReactNode;
+	config?: Parameters<typeof createConfig>[0];
 };
 
 /**
  * Provider to automatically connect to the EVM chain with the EVM address for the current BTC wallet.
  */
-export const WagmiMidlProvider = ({
-	chain: customChain,
-}: WagmiMidlProviderProps) => {
+export const WagmiAutoConnect = () => {
 	const evmAddress = useEVMAddress();
-	const chain = useEVMChain({ chain: customChain });
+	const chain = useEVMChain();
 	const { switchChain } = useSwitchChain();
 	const { connect } = useConnect();
 	const { address } = useAccount();
@@ -48,4 +59,35 @@ export const WagmiMidlProvider = ({
 	}, [evmAddress, connector, address, connect, chain, switchChain]);
 
 	return null;
+};
+
+export const WagmiMidlProvider = ({
+	chain,
+	children,
+	config: customConfig,
+}: WagmiMidlProviderProps) => {
+	const { network } = useConfig();
+
+	const config = useMemo(() => {
+		const evmChain = chain ?? getEVMFromBitcoinNetwork(network);
+
+		return createConfig(
+			customConfig ?? {
+				chains: [evmChain],
+				transports: {
+					[midlRegtest.id]: http(evmChain.rpcUrls.default.http[0]),
+				},
+				multiInjectedProviderDiscovery: false,
+				ssr: false,
+			},
+		);
+	}, [chain, customConfig, network]);
+
+	return (
+		<WagmiProvider config={config}>
+			<WagmiAutoConnect />
+
+			{children}
+		</WagmiProvider>
+	);
 };
