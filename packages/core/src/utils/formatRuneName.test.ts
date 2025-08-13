f513a68a125ee009aaf2941137ef791529891b64
annazhlb <annacher714@gmail.com>
2025-08-13 10:02:24 +0100

    test: (format rune name): update tests name, add new tests

--- packages/core/src/utils/formatRuneName.test.ts
+++ packages/core/src/utils/formatRuneName.test.ts
@@ -2,11 +2,12 @@ import { describe, expect, it } from "vitest";
 import { formatRuneName } from "./formatRuneName";
 
 describe("core | utils | formatRuneName", () => {
-	it("throws error if name is empty", () => {
+	it("throws error if name is empty string", () => {
+		expect(() => formatRuneName("")).toThrow("Rune name cannot be empty");
 		expect(() => formatRuneName(" ")).toThrow("Rune name cannot be empty");
 	});
 
-	it("throws error if rune name contains non-ASCII chars", () => {
+	it("throws error if rune name contains non-allowed characters (A-Z and •)", () => {
 		expect(() => formatRuneName("!A")).toThrowError(
 			"Rune name can only contain A-Z and • placed between letters",
 		);
@@ -20,27 +21,55 @@ describe("core | utils | formatRuneName", () => {
 		);
 	});
 
-	it("throws error if rune name contains 2 or more • in a row", () => {
-		expect(() => formatRuneName("A•B••C")).toThrowError(
+	it("throws error if rune name contains • is not located between letters", () => {
+		expect(() => formatRuneName("B••C")).toThrowError(
+			"Rune name can only contain A-Z and • placed between letters",
+		);
+		expect(() => formatRuneName("•B•C")).toThrowError(
+			"Rune name can only contain A-Z and • placed between letters",
+		);
+		expect(() => formatRuneName("B•C•")).toThrowError(
 			"Rune name can only contain A-Z and • placed between letters",
 		);
 	});
 
-	it("returns rune name if name contains only letters", () => {
-		expect(formatRuneName("AAAA")).toBe("AAAA");
+	it("returns rune name if name contains only 1 letter", () => {
+		expect(formatRuneName("A")).toBe("A");
 	});
 
-	it("returns rune name without • if name contains letters and •", () => {
+	it("returns rune name with • if name contains letters and •", () => {
 		expect(formatRuneName("RUNE•NAME")).toBe("RUNE•NAME");
 	});
 
-	it("returns rune name in local upper case format", () => {
+	it("returns rune name in upper case format", () => {
 		expect(formatRuneName("Rune•Name")).toBe("RUNE•NAME");
+	});
+
+	it("returns the name of the rune with spaces removed before and after the name", () => {
+		expect(formatRuneName("     RUNE•NAME    ")).toBe("RUNE•NAME");
+		expect(formatRuneName("     RUNE•NAME")).toBe("RUNE•NAME");
+		expect(formatRuneName("RUNE•NAME    ")).toBe("RUNE•NAME");
 	});
 
 	it("throws error if length of the rune name is more than 26 letters", async () => {
 		expect(() =>
 			formatRuneName("IN•THIS•RUNE•NAME•TWENTY•SEVEN•SY"),
 		).toThrowError("Rune name can contain no more than 26 letters");
+	});
+
+	it("returns rune name if length contains 26 letter", async () => {
+		expect(formatRuneName("IN•THIS•RUNE•NAME•TWENTY•SIX•SYM")).toBe(
+			"IN•THIS•RUNE•NAME•TWENTY•SIX•SYM",
+		);
+	});
+
+	it("throws error if rune name contains similar A-Z and • symbols", () => {
+		expect(() => formatRuneName("RUNЕ•NAME")).toThrowError(
+			"Rune name can only contain A-Z and • placed between letters",
+		);
+
+		expect(() => formatRuneName("RUNE.NAME")).toThrowError(
+			"Rune name can only contain A-Z and • placed between letters",
+		);
 	});
 });
