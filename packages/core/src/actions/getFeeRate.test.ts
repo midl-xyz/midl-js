d9f2b696724632ec22b0eefbf8b818b959112db6
annazhlb <annacher714@gmail.com>
2025-09-08 10:55:36 +0100

    test(core): mock API call in tests for getFeeRate

--- packages/core/src/actions/getFeeRate.test.ts
+++ packages/core/src/actions/getFeeRate.test.ts
@@ -1,20 +1,47 @@
-import { describe, expect, it } from "vitest";
+import { http, HttpResponse, type RequestHandler } from "msw";
+import { setupServer } from "msw/node";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 import { createConfig } from "~/createConfig";
 import { mainnet, regtest, testnet4 } from "~/networks";
 import { getFeeRate } from "./getFeeRate";
 
+const expectedFeeRateMainnet = "87654";
+const expectedFeeRateTestnet = "2345";
+const expectedFeeRateRegtest = "25";
+
+const handlers: RequestHandler[] = [
+	http.get("https://mempool.space/api/v1/fees/recommended", () => {
+		return HttpResponse.json(expectedFeeRateMainnet);
+	}),
+
+	http.get("https://mempool.space/testnet4/api/v1/fees/recommended", () => {
+		return HttpResponse.json(expectedFeeRateTestnet);
+	}),
+
+	http.get("https://mempool.regtest.midl.xyz/api/v1/fees/recommended", () => {
+		return HttpResponse.json(expectedFeeRateRegtest);
+	}),
+];
+
+const server = setupServer(...handlers);
+
 describe("core | actions | getFeeRate", () => {
+	beforeAll(() => {
+		server.listen();
+	});
+
+	afterAll(() => {
+		server.close();
+	});
+
 	it("returns the fee rate for mainnet correctly", async () => {
 		const config = createConfig({
 			networks: [mainnet],
 			connectors: [],
 		});
 		const currentFeeRate = await getFeeRate(config);
-		const estimateFeeRate = await (
-			await fetch("https://mempool.space/api/v1/fees/recommended")
-		).json();
 
-		expect(currentFeeRate).toEqual(estimateFeeRate);
+		expect(currentFeeRate).toEqual(expectedFeeRateMainnet);
 	});
 
 	it("returns the fee rate for testnet correctly", async () => {
@@ -23,11 +50,7 @@ describe("core | actions | getFeeRate", () => {
 			connectors: [],
 		});
 		const currentFeeRate = await getFeeRate(config);
-		const estimateFeeRate = await (
-			await fetch("https://mempool.space/testnet4/api/v1/fees/recommended")
-		).json();
-
-		expect(currentFeeRate).toEqual(estimateFeeRate);
+		expect(currentFeeRate).toEqual(expectedFeeRateTestnet);
 	});
 
 	it("returns the fee rate for regtest correctly", async () => {
@@ -36,11 +59,8 @@ describe("core | actions | getFeeRate", () => {
 			connectors: [],
 		});
 		const currentFeeRate = await getFeeRate(config);
-		const estimateFeeRate = await (
-			await fetch("https://mempool.regtest.midl.xyz/api/v1/fees/recommended")
-		).json();
 
-		expect(currentFeeRate).toEqual(estimateFeeRate);
+		expect(currentFeeRate).toEqual(expectedFeeRateRegtest);
 	});
 
 	it("throws error if config doesn't contains network", async () => {
