d5ca2d94826a8d0eff93376053c6f50f04ea19f7
annazhlb <167912114+annazhlb@users.noreply.github.com>
2025-09-30 11:09:14 +0100

    test(core): add unit tests for formatRuneName (#176)
    * test: (formatRuneName): add tests
    * fix(core): align formatRuneName with spec
    
    ---------
    
    Co-authored-by: Alex Novak <heyalexnovak@gmail.com>

--- packages/core/src/utils/formatRuneName.ts
+++ packages/core/src/utils/formatRuneName.ts
@@ -5,10 +5,16 @@ export const formatRuneName = (name: string) => {
 		throw new Error("Rune name cannot be empty");
 	}
 
-	const allowedCharacters = /^[A-Z•]+$/;
+	const allowedCharacters = /^[A-Z]+(•[A-Z]+)*$/;
+
+	if (runeName.replace(/•/g, "").length > 26) {
+		throw new Error("Rune name can contain up to 26 letters");
+	}
 
 	if (!allowedCharacters.test(runeName)) {
-		throw new Error("Rune name can only contain A-Z and •");
+		throw new Error(
+			"Rune name can only contain A-Z and • placed between letters",
+		);
 	}
 
 	return runeName;
