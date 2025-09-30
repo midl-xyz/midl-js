0ef7f148aa3b375a14e0fca611f7e021157c0359
annazhlb <annacher714@gmail.com>
2025-09-30 11:45:25 +0100

    test(core): update tests for getFeeRate

--- packages/core/src/actions/getFeeRate.test.ts
+++ packages/core/src/actions/getFeeRate.test.ts
@@ -1,73 +1,34 @@
-import { http, HttpResponse, type RequestHandler } from "msw";
-import { setupServer } from "msw/node";
-import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { describe, expect, it } from "vitest";
+import { mockProvider } from "~/__tests__/provider";
 import { createConfig } from "~/createConfig";
-import { mainnet, regtest, testnet4 } from "~/networks";
+import { regtest } from "~/networks";
 import { getFeeRate } from "./getFeeRate";
 
-const expectedFeeRateMainnet = "87654";
-const expectedFeeRateTestnet = "2345";
-const expectedFeeRateRegtest = "25";
-
-const handlers: RequestHandler[] = [
-	http.get("https://mempool.space/api/v1/fees/recommended", () => {
-		return HttpResponse.json(expectedFeeRateMainnet);
-	}),
-
-	http.get("https://mempool.space/testnet4/api/v1/fees/recommended", () => {
-		return HttpResponse.json(expectedFeeRateTestnet);
-	}),
-
-	http.get("https://mempool.regtest.midl.xyz/api/v1/fees/recommended", () => {
-		return HttpResponse.json(expectedFeeRateRegtest);
-	}),
-];
-
-const server = setupServer(...handlers);
-
 describe("core | actions | getFeeRate", () => {
-	beforeAll(() => {
-		server.listen();
-	});
-
-	afterAll(() => {
-		server.close();
-	});
-
-	it("returns the fee rate for mainnet correctly", async () => {
-		const config = createConfig({
-			networks: [mainnet],
-			connectors: [],
-		});
-		const currentFeeRate = await getFeeRate(config);
-
-		expect(currentFeeRate).toEqual(expectedFeeRateMainnet);
-	});
-
-	it("returns the fee rate for testnet correctly", async () => {
-		const config = createConfig({
-			networks: [testnet4],
-			connectors: [],
-		});
-		const currentFeeRate = await getFeeRate(config);
-		expect(currentFeeRate).toEqual(expectedFeeRateTestnet);
-	});
-
-	it("returns the fee rate for regtest correctly", async () => {
+	it("returns the fee rate correctly", async () => {
 		const config = createConfig({
 			networks: [regtest],
 			connectors: [],
+			provider: mockProvider,
 		});
-		const currentFeeRate = await getFeeRate(config);
 
-		expect(currentFeeRate).toEqual(expectedFeeRateRegtest);
+		const feeRate = "12";
+
+		mockProvider.getFeeRate.mockResolvedValue(feeRate);
+
+		const result = await getFeeRate(config);
+		expect(result).to.be.equal(feeRate);
 	});
 
 	it("throws error if config doesn't contains network", async () => {
 		const config = createConfig({
 			networks: [],
 			connectors: [],
+			provider: mockProvider,
 		});
-		await expect(getFeeRate(config)).rejects.toThrowError("No network");
+
+		mockProvider.getFeeRate.mockRejectedValue(new Error("No network"));
+
+		await expect(getFeeRate(config)).rejects.toThrow("No network");
 	});
 });
