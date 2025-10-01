7a359392ec36b2789124971c9b20498095888fef
annazhlb <167912114+annazhlb@users.noreply.github.com>
2025-10-01 22:58:04 +0100

    test(core): update tests for getUTXOs (#216)

--- packages/core/src/actions/getUTXOs.test.ts
+++ packages/core/src/actions/getUTXOs.test.ts
@@ -1,46 +1,51 @@
-import * as bitcoin from "bitcoinjs-lib";
-import ECPairFactory from "ecpair";
-import ecc from "@bitcoinerlab/secp256k1";
-import {
-	afterEach,
-	assert,
-	beforeAll,
-	beforeEach,
-	describe,
-	expect,
-	it,
-	vi,
-	type Mock,
-} from "vitest";
+import { describe, expect, it } from "vitest";
+import { mockProvider } from "~/__tests__/provider";
 import { getUTXOs } from "~/actions/getUTXOs";
 import { createConfig } from "~/createConfig";
 import { regtest } from "~/networks";
-import { mockServer } from "~/__tests__/mockServer";
 
-const ECPair = ECPairFactory(ecc);
+const address =
+	"bc1p3d3z4r6h2r9v5g8j7f4d2q1c0b8e7a6k5j4l3m2n1p0q9r8s7t6u5v4w3x2y1z0a9b8c7";
 
 describe("core | actions | getUTXOs", () => {
-	const keyPair = ECPair.makeRandom();
-
-	const { address } = bitcoin.payments.p2pkh({
-		pubkey: keyPair.publicKey,
-		network: bitcoin.networks.regtest,
-	});
-
-	assert(address);
-
-	beforeAll(() => {
-		mockServer.listen();
-	});
-
-	it("should get UTXOs", async () => {
+	it("returns the UTXO", async () => {
 		const config = createConfig({
 			networks: [regtest],
 			connectors: [],
+			provider: mockProvider,
 		});
 
-		const result = await getUTXOs(config, address);
+		const utxo = {
+			txid: "76546tuykhjhgftr67897656erfdghguy8u9oipklnmjhkiu",
+			vout: 0,
+			value: 100000000,
+			status: {
+				confirmed: true,
+				block_height: 0,
+				block_hash:
+					"657terdfgvbgfhytu768uiojknjhuyi756467879809opljkjhfyut76879809",
+				block_time: 0,
+			},
+		};
 
-		expect(result).toBeInstanceOf(Array);
+		mockProvider.getUTXOs.mockResolvedValue(utxo);
+
+		const result = await getUTXOs(config, address, true);
+
+		expect(result).to.be.equal(utxo);
+	});
+
+	it("throws error if the provider fails", async () => {
+		const config = createConfig({
+			networks: [regtest],
+			connectors: [],
+			provider: mockProvider,
+		});
+
+		mockProvider.getUTXOs.mockRejectedValue(new Error("Failed to fetch UTXOs"));
+
+		await expect(getUTXOs(config, address)).rejects.toThrow(
+			"Failed to fetch UTXOs",
+		);
 	});
 });
