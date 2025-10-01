ea9da1b6e358a9e34cb00b1f37f53524be3aae87
annazhlb <annacher714@gmail.com>
2025-10-01 11:18:59 +0100

    test(core): update tests for getBlockNumber

--- packages/core/src/actions/getBlockNumber.test.ts
+++ packages/core/src/actions/getBlockNumber.test.ts
@@ -1,61 +1,38 @@
-import { http, HttpResponse, type RequestHandler } from "msw";
-import { setupServer } from "msw/node";
-import { afterAll, beforeAll, describe, expect, it } from "vitest";
-import { mockServer } from "~/__tests__/mockServer";
+import { describe, expect, it } from "vitest";
+import { mockProvider } from "~/__tests__/provider";
 import { createConfig } from "~/createConfig";
-import { mainnet, regtest, testnet4 } from "~/networks";
+import { regtest } from "~/networks";
 import { getBlockNumber } from "./getBlockNumber";
 
-const expectedBlockNumberTestnet = 2345;
-const expectedBlockNumberMainnet = 87654;
-
-const handlers: RequestHandler[] = [
-	http.get("https://mempool.space/api/blocks/tip/height", () => {
-		return HttpResponse.text(expectedBlockNumberMainnet.toString());
-	}),
-
-	http.get("https://mempool.space/testnet4/api/blocks/tip/height", () => {
-		return HttpResponse.text(expectedBlockNumberTestnet.toString());
-	}),
-];
-
-const server = setupServer(...handlers);
-
 describe("core | actions | getBlockNumber", () => {
-	beforeAll(() => {
-		server.listen();
-	});
-
-	afterAll(() => {
-		server.close();
-	});
-
-	it("returns the correct mainnet block number", async () => {
-		const config = createConfig({
-			networks: [mainnet],
-			connectors: [],
-		});
-
-		expect(await getBlockNumber(config)).toEqual(expectedBlockNumberMainnet);
-	});
-
-	it("returns the correct testnet block number", async () => {
-		const config = createConfig({
-			networks: [testnet4],
-			connectors: [],
-		});
-
-		expect(await getBlockNumber(config)).toEqual(expectedBlockNumberTestnet);
-	});
-
-	it("returns the correct regtest block number", async () => {
-		mockServer.listen();
-
+	it("returns the correct block number", async () => {
 		const config = createConfig({
 			networks: [regtest],
 			connectors: [],
+			provider: mockProvider,
 		});
 
-		expect(await getBlockNumber(config)).toEqual(0);
+		const blockNumber = "1456782";
+
+		mockProvider.getLatestBlockHeight.mockResolvedValue(blockNumber);
+
+		const result = await getBlockNumber(config);
+		expect(result).to.be.equal(blockNumber);
+	});
+
+	it("throws error if the provider fails", async () => {
+		const config = createConfig({
+			networks: [regtest],
+			connectors: [],
+			provider: mockProvider,
+		});
+
+		mockProvider.getLatestBlockHeight.mockRejectedValue(
+			new Error("Failed to get block number"),
+		);
+
+		await expect(getBlockNumber(config)).rejects.toThrow(
+			"Failed to get block number",
+		);
 	});
 });
