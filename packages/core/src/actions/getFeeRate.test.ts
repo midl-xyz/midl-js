fb4bdc8f52d4cd432d89065817b59ace25d35535
annazhlb <167912114+annazhlb@users.noreply.github.com>
2025-10-01 11:06:21 +0100

    test(core): add tests for getFeeRate (#196)

---
+++ packages/core/src/actions/getFeeRate.test.ts
@@ -0,0 +1,46 @@
+import { describe, expect, it } from "vitest";
+import { mockProvider } from "~/__tests__/provider";
+import { createConfig } from "~/createConfig";
+import { regtest } from "~/networks";
+import { getFeeRate } from "./getFeeRate";
+
+describe("core | actions | getFeeRate", () => {
+	it("returns the fee rate correctly", async () => {
+		const config = createConfig({
+			networks: [regtest],
+			connectors: [],
+			provider: mockProvider,
+		});
+
+		const feeRate = "12";
+
+		mockProvider.getFeeRate.mockResolvedValue(feeRate);
+
+		const result = await getFeeRate(config);
+		expect(result).to.be.equal(feeRate);
+	});
+
+	it("throws error if the provider fails", async () => {
+		const config = createConfig({
+			networks: [regtest],
+			connectors: [],
+			provider: mockProvider,
+		});
+
+		mockProvider.getFeeRate.mockRejectedValue(
+			new Error("Failed to get fee rate"),
+		);
+
+		await expect(getFeeRate(config)).rejects.toThrow("Failed to get fee rate");
+	});
+
+	it("throws error if config doesn't contains network", async () => {
+		const config = createConfig({
+			networks: [],
+			connectors: [],
+			provider: mockProvider,
+		});
+
+		await expect(getFeeRate(config)).rejects.toThrow("No network");
+	});
+});
