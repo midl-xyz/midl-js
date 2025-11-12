7f7db17ab4bf1ccdc358fd35fa3060f0e962f127
annazhlb <annacher714@gmail.com>
2025-11-12 11:44:00 +0000

    test(core): add case for getDefaultAccount

--- packages/core/src/actions/getDefaultAccount.test.ts
+++ packages/core/src/actions/getDefaultAccount.test.ts
@@ -8,6 +8,9 @@ import { getDefaultAccount } from "./getDefaultAccount";
 
 describe("core | actions | getDefaultAccount", async () => {
 	const { keyPairConnector } = await import("@midl/node");
+	const ordinalsAddress =
+		"bcrt1puwn2akldaf2hqv64kmkjt3lgutk4se8rlmr8rcpk2v0ygg6zqqtqzzjdq9";
+	const paymentAddress = "bcrt1qz4yz7junaupmav0ycmwheglahya7wuy0g7n6tc";
 
 	it("throws error if no connection", () => {
 		const config = createConfig({
@@ -18,7 +21,7 @@ describe("core | actions | getDefaultAccount", async () => {
 		expect(() => getDefaultAccount(config)).toThrow(WalletConnectionError);
 	});
 
-	it("returns default account correctly", async () => {
+	it("returns default payment account correctly if purposes is payment", async () => {
 		const config = createConfig({
 			networks: [regtest],
 			connectors: [
@@ -33,9 +36,62 @@ describe("core | actions | getDefaultAccount", async () => {
 		});
 
 		const account = getDefaultAccount(config);
-		expect(account.address).to.be.equal(
-			"bcrt1qz4yz7junaupmav0ycmwheglahya7wuy0g7n6tc",
-		);
+		expect(account.address).to.be.equal(paymentAddress);
+	});
+
+	it("returns default ordinals account correctly if purposes is ordinals", async () => {
+		const config = createConfig({
+			networks: [regtest],
+			connectors: [
+				keyPairConnector({
+					mnemonic: __TEST__MNEMONIC__,
+				}),
+			],
+		});
+
+		await connect(config, {
+			purposes: [AddressPurpose.Ordinals],
+		});
+
+		const account = getDefaultAccount(config);
+		expect(account.address).to.be.equal(ordinalsAddress);
+	});
+
+	it("returns default payment account correctly if purposes is both payment and ordinals", async () => {
+		const config = createConfig({
+			networks: [regtest],
+			connectors: [
+				keyPairConnector({
+					mnemonic: __TEST__MNEMONIC__,
+				}),
+			],
+		});
+
+		await connect(config, {
+			purposes: [AddressPurpose.Ordinals, AddressPurpose.Payment],
+		});
+
+		const account = getDefaultAccount(config);
+		expect(account.address).to.be.equal(paymentAddress);
+	});
+
+	it("returns default ordinals account correctly if default purpose is set to ordinals", async () => {
+		const config = createConfig({
+			networks: [regtest],
+			connectors: [
+				keyPairConnector({
+					mnemonic: __TEST__MNEMONIC__,
+				}),
+			],
+			defaultPurpose: AddressPurpose.Ordinals,
+		});
+
+		await connect(config, {
+			purposes: [AddressPurpose.Payment, AddressPurpose.Ordinals],
+		});
+
+		const account = getDefaultAccount(config);
+		expect(account.address).to.be.equal(ordinalsAddress);
 	});
 
 	it("returns the correct account matching the predicate", async () => {
@@ -52,9 +108,7 @@ describe("core | actions | getDefaultAccount", async () => {
 			config,
 			(account) => account.purpose === AddressPurpose.Ordinals,
 		);
-		expect(expectedAccount.address).to.be.equal(
-			"bcrt1puwn2akldaf2hqv64kmkjt3lgutk4se8rlmr8rcpk2v0ygg6zqqtqzzjdq9",
-		);
+		expect(expectedAccount.address).to.be.equal(ordinalsAddress);
 	});
 
 	it("throws error if no match predicate", async () => {
