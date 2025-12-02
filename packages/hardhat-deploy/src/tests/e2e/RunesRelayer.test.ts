d1858deba6498847584f1e9b96dbe59b0245b958
Alex Novak <heyalexnovak@gmail.com>
2025-12-02 15:41:11 +0000

    test: remove tests

--- packages/hardhat-deploy/src/tests/e2e/RunesRelayer.test.ts
+++ packages/hardhat-deploy/src/tests/e2e/RunesRelayer.test.ts
@@ -1,13 +1,4 @@
-import {
-	SystemContracts,
-	addRuneERC20Intention,
-	executorAbi,
-	finalizeBTCTransaction,
-	getCreate2RuneAddress,
-	runeIdToBytes32,
-	signIntention,
-} from "@midl/executor";
-import { readContract, waitForTransactionReceipt } from "viem/actions";
+import { zeroAddress } from "viem";
 import { describe, it } from "vitest";
 import { useEnvironment } from "../../../tests/useEnvironment";
 
@@ -16,69 +7,15 @@ const isE2ETest = Boolean(process.env.E2E);
 describe.runIf(isE2ETest)("RunesRelayer", async () => {
 	useEnvironment(true);
 
-	it("adds rune", async () => {
+	it("deploys RunesRelayer contract", async () => {
 		const {
 			hre: { midl },
 		} = globalThis;
+
 		await midl.initialize();
-
-		// biome-ignore lint/style/noNonNullAssertion: <explanation>
-		const config = midl.getConfig()!;
-		const walletClient = await midl.getWalletClient();
-		const runeId = "21779:1";
-
-		const tx = await addRuneERC20Intention(config, runeId);
-
-		const btcTx = await finalizeBTCTransaction(config, [tx], walletClient, {
-			skipEstimateGas: true,
+		await midl.deploy("RunesRelayer", {
+			args: [zeroAddress],
 		});
-
-		const signedIntention = await signIntention(
-			config,
-			walletClient,
-			tx,
-			[tx],
-			{
-				txId: btcTx.tx.id,
-			},
-		);
-
-		console.log(
-			`Rune ERC20 create2 address will be: ${getCreate2RuneAddress(runeId)}`,
-		);
-
-		const txs = await walletClient.sendBTCTransactions({
-			btcTransaction: btcTx.tx.hex,
-			serializedTransactions: [signedIntention],
-		});
-
-		for (const tx of txs) {
-			console.log(`Waiting for tx ${tx} to be mined...`);
-			await waitForTransactionReceipt(walletClient, {
-				hash: tx,
-			});
-			console.log(`Transaction ${tx} mined.`);
-		}
-
-		const [address] = await readContract(walletClient, {
-			abi: executorAbi,
-			address: SystemContracts.Executor,
-			functionName: "getAssetAddressByRuneId",
-			args: [runeIdToBytes32(runeId)],
-		});
-
-		console.log("Rune ERC20 Address:", address);
+		await midl.execute();
 	});
-
-	// it("deploys RunesRelayer contract", async () => {
-	// 	const {
-	// 		hre: { midl },
-	// 	} = globalThis;
-
-	// 	await midl.initialize();
-	// 	await midl.deploy("RunesRelayer", {
-	// 		args: ["0x86c2122eFdfe6eA9F7a69c34aDb0D8BCa70bdbea"],
-	// 	});
-	// 	await midl.execute();
-	// });
 });
