8a4aaa760f54176a6a9921ece77f57d2e0aef754
Alex Novak <heyalexnovak@gmail.com>
2026-01-20 18:05:20 +0000

    fix(core): psbt inputs edict rune non-p2tr (#266)

--- packages/core/src/actions/edictRune.test.ts
+++ packages/core/src/actions/edictRune.test.ts
@@ -15,58 +15,59 @@ import * as mod from "./getRuneUTXO";
 describe("core | actions | edictRune", async () => {
 	const { keyPairConnector } = await import("@midl/node");
 
-	let config: Config;
-
 	beforeAll(async () => {
 		mockServer.listen();
-
-		config = createConfig({
-			networks: [regtest],
-			connectors: [keyPairConnector({ mnemonic: __TEST__MNEMONIC__ })],
-		});
-
-		await connect(config, { purposes: [AddressPurpose.Ordinals] });
 	});
 
 	afterAll(() => {
 		mockServer.close();
 	});
 
-	it.skip("should throw if more than 2 edicts", async () => {
+	it("should create correct PSBT", async () => {
+		const config = createConfig({
+			networks: [regtest],
+			connectors: [keyPairConnector({ mnemonic: __TEST__MNEMONIC__ })],
+		});
+
+		await connect(config, { purposes: [AddressPurpose.Ordinals] });
+
 		const mock = vi
 			.spyOn(mod, "getRuneUTXO")
 			.mockImplementation(async () => [
 				makeRuneUTXO("1:1", 100n, 0),
-				makeRuneUTXO("2:1", 100n, 0),
-				makeRuneUTXO("3:1", 100n, 0),
+				makeRuneUTXO("1:1", 100n, 0),
 			]);
 
-		await expect(() =>
-			edictRune(config, {
-				transfers: [
-					{
-						runeId: "1:1",
-						amount: 100n,
-						receiver: makeRandomAddress(bitcoin.networks.regtest),
-					},
-					{
-						runeId: "2:1",
-						amount: 100n,
-						receiver: makeRandomAddress(bitcoin.networks.regtest),
-					},
-					{
-						runeId: "3:1",
-						amount: 100n,
-						receiver: makeRandomAddress(bitcoin.networks.regtest),
-					},
-				],
-			}),
-		).rejects.toThrowError("Only two edicts per transaction is allowed");
+		const data = await edictRune(config, {
+			transfers: [
+				{
+					runeId: "1:1",
+					amount: 200n,
+					receiver: makeRandomAddress(bitcoin.networks.regtest),
+				},
+			],
+		});
+
+		const psbt = bitcoin.Psbt.fromBase64(data.psbt, {
+			network: bitcoin.networks.regtest,
+		});
+
+		const stone = Runestone.decipher(psbt.extractTransaction().toHex());
+
+		expect(stone.value()?.edicts.length).toBe(1);
+		expect(stone.value()?.edicts[0].amount).toBe(200n);
 
 		mock.mockRestore();
 	});
 
-	it("should create correct PSBT", async () => {
+	it("edicts with non Ordinals account", async () => {
+		const config = createConfig({
+			networks: [regtest],
+			connectors: [keyPairConnector({ mnemonic: __TEST__MNEMONIC__ })],
+		});
+
+		await connect(config, { purposes: [AddressPurpose.Payment] });
+
 		const mock = vi
 			.spyOn(mod, "getRuneUTXO")
 			.mockImplementation(async () => [
