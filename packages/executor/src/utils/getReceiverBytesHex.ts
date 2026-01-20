3191d2331395eaa430e3e78e3d1bf522f55e35b1
Alex Novak <heyalexnovak@gmail.com>
2026-01-20 13:18:35 +0000

    feat(executor): p2wpkh receiver runes (#254)
    * feat(executor): p2wpkh receiver runes
    
    * test: add args verification

---
+++ packages/executor/src/utils/getReceiverBytesHex.ts
@@ -0,0 +1,47 @@
+import { type Account, AddressType, type BitcoinNetwork } from "@midl/core";
+import * as bitcoin from "bitcoinjs-lib";
+import { padBytes, toHex } from "viem";
+import { getPublicKey } from "~/actions";
+
+export const getReceiverBytesHex = (
+	account: Account,
+	network: BitcoinNetwork,
+) => {
+	switch (account.addressType) {
+		case AddressType.P2TR: {
+			return getPublicKey(account, network) as `0x${string}`;
+		}
+
+		case AddressType.P2SH_P2WPKH: {
+			const p2wpkh = bitcoin.payments.p2wpkh({
+				pubkey: Buffer.from(account.publicKey, "hex"),
+				network: bitcoin.networks[network.network],
+			});
+			const p2sh = bitcoin.payments.p2sh({
+				redeem: p2wpkh,
+				network: bitcoin.networks[network.network],
+			});
+
+			return toHex(
+				// biome-ignore lint/style/noNonNullAssertion: P2SH output is guaranteed to be defined
+				padBytes(p2sh.output!, {
+					size: 32,
+				}),
+			);
+		}
+
+		case AddressType.P2WPKH: {
+			const p2wpkh = bitcoin.payments.p2wpkh({
+				address: account.address,
+				network: bitcoin.networks[network.network],
+			});
+
+			return toHex(
+				// biome-ignore lint/style/noNonNullAssertion: P2WPKH output is guaranteed to be defined
+				padBytes(p2wpkh.output!, {
+					size: 32,
+				}),
+			);
+		}
+	}
+};
