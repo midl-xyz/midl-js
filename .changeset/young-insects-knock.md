91bf5b82a486a833dc5bfcebd54f1337edc6f323
Alex Novak <heyalexnovak@gmail.com>
2026-01-22 13:13:27 +0000

    feat: support WIF and hex private keys in keypair connector

---
+++ .changeset/young-insects-knock.md
@@ -0,0 +1,6 @@
+---
+"@midl/core": patch
+"@midl/node": patch
+---
+
+feat(node): support WIF and hex private keys in keyPair connector
--- packages/node/src/__tests__/keyPair.ts
+++ packages/node/src/__tests__/keyPair.ts
@@ -1,2 +1,9 @@
 export const __TEST__MNEMONIC__ =
 	"test test test test test test test test test test test junk";
+
+// Test private keys (corresponding to the same mnemonic for payment address)
+export const __TEST__PRIVATE_KEY_HEX__ =
+	"c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3";
+
+export const __TEST__PRIVATE_KEY_WIF__ =
+	"cVpF924EspNh8KjYsfhgY96mmxvT6DgdWiTYMtMjuM74hJaU5psW";
